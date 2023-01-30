389c3b48a718612fddb5eab75e6db9cdf6557573
eve <eve.guerard90@gmail.com>
2023-01-30 17:14:47 +0100

    create the button sizes

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, ElementRef } from '@angular/core';
+enum Size {
+  small = "small",
+  medium = "medium",
+  large = "large"
+}
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+  constructor(private elRef:ElementRef){}
+
   @Input() buttonText: string = "hello"
+  @Input() size: string = Size.small
   
+  ngAfterContentInit(){
+    console.log()
+    if (this.size == Size.small){
+    }else if(this.size == Size.medium){
+      this.elRef.nativeElement.children[0].style.setProperty("padding","10px")
+      this.elRef.nativeElement.children[0].style.setProperty("padding","20px")
+      this.elRef.nativeElement.children[0].style.setProperty("font-size","30px")
+    }else if(this.size == Size.large){
+      this.elRef.nativeElement.children[0].style.setProperty("padding","20px")
+      this.elRef.nativeElement.children[0].style.setProperty("padding","40px")
+      this.elRef.nativeElement.children[0].style.setProperty("font-size","50px")
+    }
+  }
 }
 
